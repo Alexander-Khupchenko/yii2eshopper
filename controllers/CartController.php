2b766b011603e4fcb9ba86947e78682966bf6b4b
Oleksander <khupchenko86@gmail.com>
2016-12-12 23:25:27 +0200

    поиск по сайту, корзина покупателя(начало работы)

---
+++ controllers/CartController.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\controllers;
+use app\models\Product;
+use app\models\Cart;
+use Yii;
+
+class CartController extends AppController {
+    
+    public function actionAdd() {
+        $id = Yii::$app->request->get('id');
+        $product = Product::findOne($id);
+        if(empty($product)) return false;
+        $session = Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->addToCart($product);
+    }
+    
+}
